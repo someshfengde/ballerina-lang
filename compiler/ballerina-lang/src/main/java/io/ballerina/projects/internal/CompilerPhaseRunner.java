19f0e143c28d2b69830ee76d84f0f4248e6c45b9
Thushara-Piyasekara <thusharap@wso2.com>
2023-12-20 08:28:01 +0530

    Add unused module elimination

--- compiler/ballerina-lang/src/main/java/io/ballerina/projects/internal/CompilerPhaseRunner.java
+++ compiler/ballerina-lang/src/main/java/io/ballerina/projects/internal/CompilerPhaseRunner.java
@@ -167,7 +167,7 @@ public class CompilerPhaseRunner {
             return;
         }
 
-        deadBirNodeAnalyze(pkgNode);
+//        deadBirNodeAnalyze(pkgNode);
         if (this.stopCompilation(pkgNode, CompilerPhase.BIR_EMIT)) {
             return;
         }
