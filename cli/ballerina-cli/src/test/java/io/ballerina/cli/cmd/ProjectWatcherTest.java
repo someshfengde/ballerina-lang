c79c488c57b06f47cb3ea8023e301f858cb68326
Gayal Dassanayake <g.c.dassanayake@gmail.com>
2024-08-06 14:32:38 +0530

    Add 3 tests

--- cli/ballerina-cli/src/test/java/io/ballerina/cli/cmd/ProjectWatcherTest.java
+++ cli/ballerina-cli/src/test/java/io/ballerina/cli/cmd/ProjectWatcherTest.java
@@ -7,6 +7,8 @@ import io.ballerina.projects.environment.EnvironmentBuilder;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import picocli.CommandLine;
 
@@ -31,13 +33,13 @@ import static io.ballerina.projects.util.ProjectConstants.DIST_CACHE_DIRECTORY;
 public class ProjectWatcherTest extends BaseCommandTest {
     private static final String WATCH_FLAG = "--watch";
     private static final int THREAD_SLEEP_DURATION_IN_MS = 8000;
+    private static final String PROJECT_NAME_PLACEHOLDER = "INSERT_PROJECT_NAME";
 
     private Path watchTestResources;
     private Path testDistCacheDirectory;
     private ProjectEnvironmentBuilder projectEnvironmentBuilder;
     private Thread watcherThread;
     private AtomicReference<ProjectWatcher> watcher;
-    private CountDownLatch latch = new CountDownLatch(1);
 
     // TODO: scenarios
     //  Negatives
@@ -76,18 +78,19 @@ public class ProjectWatcherTest extends BaseCommandTest {
         }
     }
 
-    @Test(description = "Run bal file containing syntax error with --watch")
-    public void testRunWatchBalFileWithSyntaxError() throws IOException, InterruptedException {
-        Path balFilePath = this.watchTestResources.resolve("service.bal");
+    @Test(description = "Run a correct bal service file and do a correct change")
+    public void testRunWatchCorrectBalFileWithCorrectChange() throws IOException, InterruptedException {
+        Path balFilePath = createTempFile("service.bal");
         RunCommand runCommand = new RunCommand(balFilePath, printStream, false);
         new CommandLine(runCommand).parseArgs(WATCH_FLAG, balFilePath.toString());
+        CountDownLatch latch = new CountDownLatch(1);
         watcherThread = new Thread(() -> {
             try {
                 watcher.set(new ProjectWatcher(runCommand, balFilePath, printStream));
                 latch.countDown();
                 watcher.get().watch();
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                Assert.fail("Error occurred while watching the project: " + e);
             }
         });
         watcherThread.start();
@@ -95,8 +98,59 @@ public class ProjectWatcherTest extends BaseCommandTest {
         Thread.sleep(THREAD_SLEEP_DURATION_IN_MS);
         replaceFileContent(balFilePath, this.watchTestResources.resolve("service-updated.bal"));
         Thread.sleep(THREAD_SLEEP_DURATION_IN_MS);
-        String buildLog = readOutput(true);
-        Assert.assertEquals(buildLog.replace("\r", ""), getOutput("watch-single-file-service.txt"));
+        String actualOutput = readOutput(true).replace("\r", "");
+        String expectedOutput = readExpectedOutputFile("watch-correct-service-file-correct-change.txt", balFilePath);
+        Assert.assertEquals(actualOutput, expectedOutput);
+    }
+
+    @Test(description = "Run a correct bal service file and do a erroneous change")
+    public void testRunWatchCorrectBalFileWithErroneousChange() throws IOException, InterruptedException {
+        Path balFilePath = createTempFile("service.bal");
+        RunCommand runCommand = new RunCommand(balFilePath, printStream, false);
+        new CommandLine(runCommand).parseArgs(WATCH_FLAG, balFilePath.toString());
+        CountDownLatch latch = new CountDownLatch(1);
+        watcherThread = new Thread(() -> {
+            try {
+                watcher.set(new ProjectWatcher(runCommand, balFilePath, printStream));
+                latch.countDown();
+                watcher.get().watch();
+            } catch (IOException e) {
+                Assert.fail("Error occurred while watching the project: " + e);
+            }
+        });
+        watcherThread.start();
+        latch.await();
+        Thread.sleep(THREAD_SLEEP_DURATION_IN_MS);
+        replaceFileContent(balFilePath, this.watchTestResources.resolve("service-error.bal"));
+        Thread.sleep(THREAD_SLEEP_DURATION_IN_MS);
+        String actualOutput = readOutput(true).replace("\r", "");
+        String expectedOutput = readExpectedOutputFile("watch-correct-service-file-error-change.txt", balFilePath);
+        Assert.assertEquals(actualOutput, expectedOutput);
+    }
+
+    @Test(description = "Run a erroneous bal service file and do a correct change")
+    public void testRunWatchErroneousBalFileWithCorrectChange() throws IOException, InterruptedException {
+        Path balFilePath = createTempFile("service-error.bal");
+        RunCommand runCommand = new RunCommand(balFilePath, printStream, false);
+        new CommandLine(runCommand).parseArgs(WATCH_FLAG, balFilePath.toString());
+        CountDownLatch latch = new CountDownLatch(1);
+        watcherThread = new Thread(() -> {
+            try {
+                watcher.set(new ProjectWatcher(runCommand, balFilePath, printStream));
+                latch.countDown();
+                watcher.get().watch();
+            } catch (IOException e) {
+                Assert.fail("Error occurred while watching the project: " + e);
+            }
+        });
+        watcherThread.start();
+        latch.await();
+        Thread.sleep(THREAD_SLEEP_DURATION_IN_MS);
+        replaceFileContent(balFilePath, this.watchTestResources.resolve("service.bal"));
+        Thread.sleep(THREAD_SLEEP_DURATION_IN_MS);
+        String actualOutput = readOutput(true).replace("\r", "");
+        String expectedOutput = readExpectedOutputFile("watch-error-service-file-correct-change.txt", balFilePath);
+        Assert.assertEquals(actualOutput, expectedOutput);
     }
 
     @AfterMethod
@@ -106,8 +160,16 @@ public class ProjectWatcherTest extends BaseCommandTest {
                 stopProjectWatcher(watcherThread, watcher.get());
             }
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            Assert.fail("Error occurred while stopping the project watcher. " +
+                    "Please kill any stale java processes that were started by the project watcher tests: " + e);
         }
+    }
+
+    private Path createTempFile(String fileName) throws IOException {
+        Path balFilePath = this.watchTestResources.resolve(fileName);
+        Path tempFilePath = Files.createTempFile("service", ".bal");
+        replaceFileContent(tempFilePath, balFilePath);
+        return tempFilePath;
     }
 
     private void replaceFileContent(Path filePath, Path copyFrom) {
@@ -117,6 +179,11 @@ public class ProjectWatcherTest extends BaseCommandTest {
         } catch (IOException e) {
             Assert.fail("Error occurred while writing to the file: " + e);
         }
+    }
+
+    private String readExpectedOutputFile(String expectedOutputFile, Path balFilePath) throws IOException {
+        return getOutput(expectedOutputFile)
+                .replace("INSERT_PROJECT_NAME", balFilePath.getFileName().toString());
     }
 
     private void stopProjectWatcher(Thread thread, ProjectWatcher watcher) throws InterruptedException {
